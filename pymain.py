0f03a3a59c48285d6c9cec7b194c95a1b2f8f1ae
david20001@gmail.com <david@86022907>
2018-11-19 13:54:34 +0800

    init

---
+++ pymain.py
@@ -0,0 +1,16 @@
+#encoding=utf-8
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+c, s = np.cos(x), np.sin(x)
+plt.figure()
+plt.plot(x, c)
+plt.plot(x, s)
+plt.show()
+
+
+
+
