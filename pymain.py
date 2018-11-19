a0eff8f0b11f12a14607723a0b00fad9869b7b26
david20001@gmail.com <david@86022907>
2018-11-19 14:26:14 +0800

    plt argus

--- pymain.py
+++ pymain.py
@@ -4,11 +4,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x = np.linspace(-np.pi, np.pi, 512, endpoint=True)
+x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
 c, s = np.cos(x), np.sin(x)
 plt.figure()
-plt.plot(x, c)
-plt.plot(x, s)
+plt.plot(x, c, color='blue', linewidth=1.0, linestyle='-', label='cos', alpha=0.5)
+plt.plot(x, s, 'r-', label='sin')
+ax=plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.spines['left'].set_position(('data', 0))
+ax.spines['bottom'].set_position(('data', 0))
+plt.title('cos and sin')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
 plt.show()
 
 
