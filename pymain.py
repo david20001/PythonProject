7dc8fa28c192227e5083f1ba6cecaeb910feec8c
david20001@gmail.com <david@86022907>
2018-11-19 14:09:52 +0800

    init

--- pymain.py
+++ pymain.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+x = np.linspace(-np.pi, np.pi, 512, endpoint=True)
 c, s = np.cos(x), np.sin(x)
 plt.figure()
 plt.plot(x, c)
