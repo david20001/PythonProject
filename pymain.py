cdf62b925191154710e0c2c55b784f0898ab50d6
david20001@gmail.com <david@86022907>
2018-11-19 16:31:39 +0800

    exmp1

--- pymain.py
+++ pymain.py
@@ -9,7 +9,7 @@ c, s = np.cos(x), np.sin(x)
 plt.figure()
 plt.plot(x, c, color='blue', linewidth=1.0, linestyle='-', label='cos', alpha=0.5)
 plt.plot(x, s, 'r-', label='sin')
-ax=plt.gca()
+ax = plt.gca()
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 ax.spines['left'].set_position(('data', 0))
@@ -17,6 +17,12 @@ ax.spines['bottom'].set_position(('data', 0))
 plt.title('cos and sin')
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
+
+plt.legend(loc='upper right')
+plt.grid()
+
+plt.fill_between(x, np.abs(x) < 0.5, c, c > 0.5, color='green', alpha=0.25)
+
 plt.show()
 
 
