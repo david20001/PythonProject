61f9ddaca0f00d6702659c6d9c7c05daa394911e
david20001@gmail.com <david@86022907>
2018-11-20 11:09:18 +0800

    测试

--- scipytest.py
+++ scipytest.py
@@ -5,3 +5,4 @@ from scipy.integrate import quad,dblquad,nquad
 print(quad(lambda x: np.exp(-x), 0, np.inf))
 print(dblquad(lambda t, x: np.exp(-x*t)/t**3, 0, np.inf, lambda x: 1, lambda x: np.inf))
 print('hello')
+print('testvcs')
